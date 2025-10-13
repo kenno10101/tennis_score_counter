6a3963040acc274305632b939126bbe064ba6f89
Kenn-Michael Sanga <kennsanga@gmail.com>
2025-10-13 17:44:53 +0200

    Initial Commit

---
+++ tennis_score_counter Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tennis_score_counter Watch App
+//
+//  Created by Kenn-Michael Sanga on 13.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tennis_score_counter Watch App/tennis_score_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  tennis_score_counterApp.swift
+//  tennis_score_counter Watch App
+//
+//  Created by Kenn-Michael Sanga on 13.10.25.
+//
+
+import SwiftUI
+
+@main
+struct tennis_score_counter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
